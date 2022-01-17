951d19fdb849a27c106ff7bc8be18be7f93738f0
Chelsea Alcinord <59550990+seasea19@users.noreply.github.com>
2022-01-17 18:38:42 -0500

    Modified

--- src/components/header_footer/Footer.js
+++ src/components/header_footer/Footer.js
@@ -3,15 +3,14 @@ import Fade from 'react-reveal/Fade';
 
 const footer = () => {
     return (
-        <Footer className="bck_red">
+        <footer className="bck_red">
             <Fade delay={500}>
-                <div className="foont_righteoud footer_logo_venue"> Openning Day</div>
+                <div className="font_righteous footer_logo_venue"> Openning Day</div>
                 <div className="footer_copyright">
                     Yankee Stadium 2022. All rights reserved.
                 </div>
             </Fade>
-        </Footer>
+        </footer>
     );
 };
-
-export default Footer;
+export default footer;
